5bb75f50f706c6cffadac7f6dc089b04f8c8638b
Gabryel Monteiro <gabryelarm@gmail.com>
2020-07-16 12:35:52 -0300

    Correcting back endusers so it return null on more than one user

--- src/models/SearchResult.kt
+++ src/models/SearchResult.kt
@@ -1,5 +1,6 @@
 package com.rafael.models
 
 class SearchResult(private val endUsers: List<EndUser>) {
-    fun uniqueResult() = endUsers.firstOrNull()
+    fun uniqueResult() =
+        if (endUsers.count() == 1) endUsers.first() else null
 }

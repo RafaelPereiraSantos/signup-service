31c01165731a0d60c9ed6cbadda83d66aaf51c86
RafaelPereiraSantos <rafael.pereira@gympass.com>
2020-07-23 18:38:43 -0300

    created a new module using spring boot to create the endpoints

---
+++ api-spring/src/main/kotlin/com/rafael/Application.kt
@@ -0,0 +1,13 @@
+package com.rafael
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main() {
+    runApplication<Application>()
+}
+
---
+++ api-spring/src/main/kotlin/com/rafael/config/ServiceConfiguration.kt
@@ -0,0 +1,24 @@
+package com.rafael.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.rafael.service.EligibilitySearch
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ServiceConfiguration {
+
+    @Bean
+    fun eligibilitySearch(): EligibilitySearch = EligibilitySearch()
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return jacksonObjectMapper().apply {
+            propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        }
+    }
+}
